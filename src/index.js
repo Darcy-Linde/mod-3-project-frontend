d34c588c5ab6a9229212a853649b85476223951f
Darcy Linde <lindedarcy@gmail.com>
2019-05-07 16:28:10 -0400

    buttons clickable. Fetches stock info

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 let br = document.createElement('br')
 
+function clearDOM(){
+  let body = document.querySelector('body')
+  body.innerHTML = ""
+}
+
 // For "signed in" user:
 //  searching for a keyword returns buttons with stock names
 //  buttons have event listeners for clicks
 //  clicking a button will render relevant info about stock
 //  user will have option to make purchase of shares
+
+// Interfacing
+// Need Sign In Page - find or create user
+// user name stored in global variable
+//
+// 
 
 function renderUser(user){
   let div = document.createElement('div')
@@ -53,8 +64,35 @@ function renderCompanies(companies){
     let symbol = company["1. symbol"]
     let name = company["2. name"]
     button.innerText = `Name: ${name} , Symbol: ${symbol}`
-    button.addEventListener('click', renderStock)
+    button.id = symbol
+    button.addEventListener('click', stockClick)
     div.appendChild(button)
   })
   document.querySelector('body').appendChild(div)
+}
+
+function stockClick(e){
+  clearDOM()
+  let symbol = e.target.id
+  fetchStock(symbol)
+}
+
+function fetchStock(symbol){
+  const stockURL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=T8E6RZ7YEO1NDYZU`
+  fetch(stockURL)
+  .then((res) => res.json())
+  .then((data) => renderStock(data["Global Quote"]))
+}
+
+function renderStock(quote){
+  let symbol = quote["01. symbol"]
+  let price = quote["05. price"]
+  let volume = quote["06. volume"]
+  let change = quote["09. change"]
+  let changePercent = quote["10. change percent"]  //these values are strings!
+  console.log(symbol)
+  console.log(price)
+  console.log(volume)
+  console.log(change)
+  console.log(changePercent)
 }
